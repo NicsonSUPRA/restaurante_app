6f696f55eae9829bf17d55f43ee2edd0bc8b6551
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-10-25 10:02:13 -0300

    inicio do projeto

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+final DUMMY_DATA = [
+  Category(id: 'c1', title: 'Italiano', color: Colors.amber),
+  Category(id: 'c2', title: 'Rapido e Fácil', color: Colors.red),
+  Category(id: 'c3', title: 'Hamburger'),
+  Category(id: 'c4', title: 'Alemã', color: Colors.purple),
+  Category(id: 'c5', title: 'Leve e Saudável', color: Colors.lightGreen),
+  Category(id: 'c6', title: 'Exótico', color: Colors.blueAccent),
+  Category(id: 'c7', title: 'Café da Manhã', color: Colors.lightBlueAccent),
+  Category(id: 'c8', title: 'Asiático', color: Colors.redAccent)
+];
